3404486b52e405bf0e6d82a34daa72a2dea2f6f9
JorelReis <jorelreis29@gmail.com>
2023-10-12 13:34:49 -0300

    my second commit

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
-//salve
+//Desafio Classificador de nível de Herói
+let heroi = {
+    nome: "Znynja",
+    quantidadeXp: 5000,
+  }
+  
+  let mensagemNivel = "";
+  
+  if (heroi.quantidadeXp <= 1000) {
+    mensagemNivel = "Ferro";
+  } else if (heroi.quantidadeXp > 1000 && heroi.quantidadeXp <= 2000) {
+    mensagemNivel = "Bronze";
+  } else if (heroi.quantidadeXp > 2000 && heroi.quantidadeXp <= 5000) {
+    mensagemNivel = "Prata";
+  } else if (heroi.quantidadeXp > 6000 && heroi.quantidadeXp <= 7000) {
+    mensagemNivel = "Ouro";
+  } else if (heroi.quantidadeXp > 7000 && heroi.quantidadeXp <= 8000) {
+    mensagemNivel = "Platina";
+  } else if (heroi.quantidadeXp > 8000 && heroi.quantidadeXp <= 9000) {
+    mensagemNivel = "Ascendente";
+  } else if (heroi.quantidadeXp > 9000 && heroi.quantidadeXp <= 10000) {
+    mensagemNivel = "Imortal";
+  } else if (heroi.quantidadeXp >= 10001) {
+    mensagemNivel = "Radiante";
+  }
+
+  //"O Herói de nome **{nome}** está no nível de **{nivel}**"
+  console.log("O Herói de nome " + heroi.nome + " está no nível de " + mensagemNivel);
+  
